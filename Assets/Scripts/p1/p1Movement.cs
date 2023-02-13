e38bbaa79f857c518d9e736ecee7f68275713ae2
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2023-02-13 14:39:13 +0000

    Added movement for player.

--- Assets/Scripts/p1/p1Movement.cs
+++ Assets/Scripts/p1/p1Movement.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class p1Movement : MonoBehaviour
 {
+    Rigidbody2D rb2d;
+
+    int movementSpeed = 3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        MovePlayerOld();   
+    }
+
+    void MovePlayerOld()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb2d.velocity = new Vector2(0, movementSpeed);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            rb2d.velocity = new Vector2(0, -movementSpeed);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb2d.velocity = new Vector2(-movementSpeed, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb2d.velocity = new Vector2(movementSpeed, 0);
+        }
+
     }
 }
