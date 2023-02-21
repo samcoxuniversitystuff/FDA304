c2a6b999bdc9aa417f8bde74823dbb62450e1d41
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2023-02-21 16:56:44 +0000

    Added in timer.

--- Assets/Scripts/Bullet/BulletDestroy.cs
+++ Assets/Scripts/Bullet/BulletDestroy.cs
@@ -5,12 +5,32 @@ using UnityEngine;
 
 public class BulletDestroy : MonoBehaviour
 {
+    float timer = 0;
+    float maxLifetime = 5f;
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Enemy"))
         {
             Destroy(col.gameObject);
-            Destroy(gameObject);
+            Destroy(this);
+        }
+    }
+
+    void Start()
+    {
+        timer = 0;
+    }
+
+    void Update()
+    {
+        print(timer);
+        if (timer < maxLifetime)
+        {
+            timer += (1);
+        }
+        else
+        {
+            Destroy(this);
         }
     }
 }
