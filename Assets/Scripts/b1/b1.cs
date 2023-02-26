e9cbc5fcf9d6570c5471b91dbe4b22d0392f165f
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2023-02-26 09:02:40 +0000

    direction code didn't work, removed it.

--- Assets/Scripts/b1/b1.cs
+++ Assets/Scripts/b1/b1.cs
@@ -10,7 +10,7 @@ public class b1 : MonoBehaviour
 {
     Rigidbody2D _rigidbody2D;
     public Vector3 BulletDirection;
-    [FormerlySerializedAs("_bulletSpeed")] [SerializeField] private float bulletSpeed = 20f;
+    [SerializeField] private float bulletSpeed = 20f;
     [SerializeField] private float buffer = 0.5f;
     float _timer = 3f;
     private void OnCollisionEnter2D(Collision2D col)
@@ -30,9 +30,7 @@ public class b1 : MonoBehaviour
 
     void Start()
     {
-        _timer = 0;
         _rigidbody2D = GetComponent<Rigidbody2D>();
-        _rigidbody2D.velocity = (transform.position * bulletSpeed);
     }
 
     void Update()
--- Assets/Scripts/p1/p1Shooting.cs
+++ Assets/Scripts/p1/p1Shooting.cs
@@ -26,43 +26,12 @@ public class p1Shooting : MonoBehaviour
 
     private void Update()
     {
-        SetShootingDirection();
         ShootBullet();
     }
 
     private void FixedUpdate()
     {
 
-    }
-
-    void SetShootingDirection()
-    {
-        _p1Direction = _p1Movement.GetPlayerDirection();
-        if (_p1Direction.x == 0 && _p1Direction.y == -1) // South.
-        {
-            fireObject.transform.position = bSouth.transform.position;
-            fireObject.transform.rotation = bSouth.transform.rotation;
-        }
-        else if (_p1Direction.x == 0 && _p1Direction.y == 1) // North.
-        {
-            fireObject.transform.position = bNorth.transform.position;
-            fireObject.transform.rotation = bNorth.transform.rotation;
-        }
-        else if (_p1Direction.x == 1 && _p1Direction.y == 0) // East.
-        {
-            fireObject.transform.position = bEast.transform.position;
-            fireObject.transform.rotation = bEast.transform.rotation;
-        }
-        else if (_p1Direction.x == -1 && _p1Direction.y == 0) // West.
-        {
-            fireObject.transform.position = bWest.transform.position;
-            fireObject.transform.rotation = bWest.transform.rotation;
-        }
-        else
-        {
-            fireObject.transform.position = bSouth.transform.position;
-            fireObject.transform.rotation = bSouth.transform.rotation;
-        }
     }
 
     void ShootBullet()
@@ -72,9 +41,6 @@ public class p1Shooting : MonoBehaviour
             GameObject _bulletOBJ = Instantiate(bullet, fireObject.transform.position, fireObject.transform.rotation) as GameObject;
             Rigidbody2D _bulletRB = _bulletOBJ.GetComponent<Rigidbody2D>();
             b1 b1Script = _bulletOBJ.GetComponent<b1>();
-            b1Script.BulletDirection = fireObject.transform.forward;
-            _bulletRB.AddForce(fireObject.transform.forward * bulletForceAmount , ForceMode2D.Impulse);
-            
         }
     }
 }
