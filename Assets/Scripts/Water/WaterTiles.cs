0a4aaa3958f53458f0cf71462f54a49783386500
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2023-03-03 08:40:04 +0000

    Added ground back in.

--- Assets/Scripts/Water/WaterTiles.cs
+++ Assets/Scripts/Water/WaterTiles.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,5 +15,14 @@ public class WaterTiles : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            p1Lives player1Lives = col.gameObject.GetComponent<p1Lives>();
+            player1Lives.SetLives(0);
+        }
     }
 }
