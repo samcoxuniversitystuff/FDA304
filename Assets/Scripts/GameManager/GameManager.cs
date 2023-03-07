cddff7bceb409b5dc1126dc74c9ee8ae2c69953b
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2023-03-07 15:41:32 +0000

    Pause using R.

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -16,7 +16,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(ResetGame());
+        }
     }
 
     public IEnumerator ResetGame()
