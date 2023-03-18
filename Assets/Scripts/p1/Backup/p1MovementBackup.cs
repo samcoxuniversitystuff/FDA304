7b6aa774c7ce2bd6ab7be69d553b9e674cea8765
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2023-03-18 16:22:22 +0000

    Added backup files, New input system.

---
+++ Assets/Scripts/p1/Backup/p1MovementBackup.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class p1MovementBackup : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb2d;
+
+    [FormerlySerializedAs("a")] [FormerlySerializedAs("_a")] [SerializeField] Animator playerAnimator;
+    [SerializeField] float movementSpeed = 3f;
+    [SerializeField] private float speedMultiplier = 2f;
+    [SerializeField] private float rushSpeed;
+
+    [SerializeField] private float originalSpeed;
+    [SerializeField] Vector2 movementDirection;
+
+    public Camera gameCam;
+    private Vector2 _mousePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        originalSpeed = movementSpeed;
+        rushSpeed = movementSpeed * speedMultiplier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMovementDirection();
+        UpdateAnimation();
+        ChoosePlayerSpeed();
+
+    }
+    
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void ChoosePlayerSpeed()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = rushSpeed;
+        }
+        else
+        {
+            movementSpeed = originalSpeed;
+        }
+    }
+
+
+    void UpdateAnimation()
+    {
+        playerAnimator.SetFloat("Horizontal", movementDirection.x);
+        playerAnimator.SetFloat("Vertical", movementDirection.y);
+        playerAnimator.SetFloat("Speed", movementDirection.sqrMagnitude);
+    }
+
+
+
+    void UpdateMovementDirection()
+    {
+        movementDirection.x = Input.GetAxisRaw("Horizontal");
+        movementDirection.y = Input.GetAxisRaw("Vertical");
+        // movementDirection = movementDirection.normalized;
+
+    }
+
+    void MovePlayer()
+    {
+        rb2d.MovePosition(rb2d.position + movementDirection * (movementSpeed * Time.fixedDeltaTime));
+    }
+
+    public Vector2 GetPlayerDirection()
+    {
+        return movementDirection;
+    }
+}
---
+++ Assets/Scripts/p1/Backup/p1ShootingBackup.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class p1ShootingBackup : MonoBehaviour
+{
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float bulletForceAmount = 10;
+
+    private p1Movement _p1Movement;
+    private float xDirection;
+    private float yDirection;
+
+    [SerializeField] private Transform pUp;
+    [SerializeField] private Transform pDown;
+    [SerializeField] private Transform pLeft;
+    [SerializeField] private Transform pRight;
+
+    [SerializeField] private Transform pUpLeft;
+    [SerializeField] private Transform pUpRight;
+    [SerializeField] private Transform pDownLeft;
+    [SerializeField] private Transform pDownRight;
+
+    public bool isShooting = true;
+
+    [SerializeField] private AudioClip bulletSound;
+    
+    private void Start()
+    {
+        _p1Movement = FindObjectOfType<p1Movement>();
+    }
+
+    private void Update()
+    {
+        xDirection = _p1Movement.GetPlayerDirection().x;
+        yDirection = _p1Movement.GetPlayerDirection().y;
+        ChangeWeaponType();
+        if (isShooting)
+        {
+            ShootBullet();
+        }
+
+    }
+
+    void ChangeWeaponType()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (isShooting)
+            {
+                isShooting = false;
+            }
+            else if (!isShooting)
+            {
+                isShooting = true;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    Vector2 GetShootingPosition()
+    {
+        switch (xDirection)
+        {
+            case 0 when yDirection == -1: // Start of first 4 coordinates. 
+                return pDown.position;
+            case 0 when yDirection == 1:
+                return pUp.position;
+            case 1 when yDirection == 0:
+                return pRight.position;
+            case -1 when yDirection == 0:
+                return pLeft.position; // End of first 4 coordinates.
+            case -1 when yDirection == 1: // Start of other 4 coordinates. 
+                return pUpLeft.position; 
+            case 1 when yDirection == 1:
+                return pUpRight.position;
+            case -1 when yDirection == -1:
+                return pDownLeft.position;
+            case 1 when yDirection == -1:
+                return pDownRight.position; // End of other 4 coordinates.
+            default:
+                return pDown.position;
+        }
+    }
+
+    Vector2 GetShootingDirection()
+    {
+        switch (xDirection)
+        {
+            case 0 when yDirection == -1: // Start of first 4 coordinates. 
+                return Vector2.down;
+            case 0 when yDirection == 1:
+                return Vector2.up;
+            case 1 when yDirection == 0:
+                return Vector2.right;
+            case -1 when yDirection == 0:
+                return Vector2.left; // End of first 4 coordinates.
+            case -1 when yDirection == 1: // Start of other 4 coordinates. 
+                return (Vector2.left + Vector2.up);
+            case 1 when yDirection == 1:
+                return (Vector2.right + Vector2.up);
+            case -1 when yDirection == -1:
+                return (Vector2.left + Vector2.down);
+            case 1 when yDirection == -1:
+                return (Vector2.right + Vector2.down); // End of other 4 coordinates.
+            default:
+                return Vector2.down;
+        }
+    }
+
+    void ShootBullet()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AudioSource.PlayClipAtPoint(bulletSound, transform.position);
+            GameObject _bulletOBJ = Instantiate(bullet, GetShootingPosition(), quaternion.identity) as GameObject;
+            Rigidbody2D _bulletRB = _bulletOBJ.GetComponent<Rigidbody2D>();
+            b1 b1Script = _bulletOBJ.GetComponent<b1>();
+            _bulletRB.velocity = GetShootingDirection() * bulletForceAmount;
+        }
+    }
+
+    public bool GetShootingMode()
+    {
+        if (isShooting)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
