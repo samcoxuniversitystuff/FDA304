e88010c7519ac2b1583b930c44a0b21c2a3dec1b
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2023-04-24 11:17:44 +0100

    Finishing up.

--- Assets/Scripts/Settings/settingsCanvas.cs
+++ Assets/Scripts/Settings/settingsCanvas.cs
@@ -1,48 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
 public class settingsCanvas : MonoBehaviour
 {
 
+
+    private Resolution[] computerResolutions;
     
     [SerializeField] private string mainMenuSceneName = "mainMenu";
 
-    [SerializeField] TMP_Dropdown FullscreenDropdown;
-    [SerializeField] TMP_Dropdown GraphicsDropdown;
     [SerializeField] TMP_Dropdown ResDropdown;
 
-    Resolution[] resOptions;
-
-
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        computerResolutions = Screen.resolutions;
         
+        ResDropdown.ClearOptions();
+
+        List<string> resOptions = new List<string>();
+
+        int PCResIndex = 0;
+
+        for (int i = 0; i < computerResolutions.Length; i++)
+        {
+            string resOption = computerResolutions[i].width + "x" + computerResolutions[i].height + "@" + computerResolutions[i].refreshRateRatio + "hz";
+            resOptions.Add(resOption);
+
+            if (computerResolutions[i].width == Screen.currentResolution.width && computerResolutions[i].height == Screen.currentResolution.height)
+            {
+                PCResIndex = i;
+            }
+        }
+        
+        ResDropdown.AddOptions(resOptions);
+        ResDropdown.value = PCResIndex;
+        ResDropdown.RefreshShownValue();
+    }
+    public void FullscreenMode(bool newFullscreenMode)
+    {
+        Screen.fullScreen = newFullscreenMode;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetGameRes(int resToSetIndex)
     {
-        
+        Resolution gameRes = computerResolutions[resToSetIndex];
+        Screen.SetResolution(gameRes.width, gameRes.height, Screen.fullScreen);
     }
 
     public void CloseSettings()
     {
         SceneManager.LoadScene(mainMenuSceneName);
-    }
-
-    public void SetScreenMode(bool OnFullscreenMode)
-    {
-        Screen.fullScreen = OnFullscreenMode;
-    }
-
-    public void SetRes(int resIndex)
-    {
-        Resolution currentRes = resOptions[resIndex];
-        Screen.SetResolution(currentRes.width, currentRes.height, Screen.fullScreen);
     }
 }
